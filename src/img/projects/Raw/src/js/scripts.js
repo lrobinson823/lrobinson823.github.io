4f55fc25e6d2ecdd22ce0ac22315e81855ccb6c6
Lamar Robinson <nw823@Lamars-MacBook-Pro.local>
2019-03-01 12:10:42 -0800

    RAW webpage added

---
+++ src/img/projects/Raw/src/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".slideshow").backstretch([
+    "src/img/1.jpg",
+    "src/img/2.jpg",
+    "src/img/3.jpg"
+  ], {
+    duration: 3000,
+    fade: 750
+  });
+
+});
